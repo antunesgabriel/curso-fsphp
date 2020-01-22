478097e0fc1e46869e5244104aba4c528eff9159
antunesgabriel <gabrielantunescontato@gmail.com>
2020-01-22 05:10:27 -0300

    :wip: video 10 modulo 7

--- 07-recursos-e-componentes/07-10-paginacao-de-resultados/index.php
+++ 07-recursos-e-componentes/07-10-paginacao-de-resultados/index.php
@@ -4,7 +4,21 @@ fullStackPHPClassName("07.10 - Paginador de resultados");
 
 require __DIR__ . "/../vendor/autoload.php";
 
+use Source\Support\Pager;
+
 /*
  * [ paginator ] https://packagist.org/packages/coffeecode/paginator
  */
+
 fullStackPHPClassSession("paginator", __LINE__);
+
+$current = filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT);
+
+$paginator = new Pager('?page=');
+
+$paginator->pager(60, 10, $current, 2);
+
+$pages = $paginator->render();
+
+
+echo $pages;
